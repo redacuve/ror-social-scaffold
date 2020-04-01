fb8145ea11380cbb8b583c37f079225a4147747e
acordova <ae_cordova@outlook.com>
2020-04-01 16:26:19 -0600

    Added integration tests for user sign in

---
+++ spec/features/user_sends_friend_invitation_spec.rb
@@ -0,0 +1,20 @@
+
+require 'rails_helper'
+
+RSpec.feature 'Vistor signs in' do
+  background do
+    User.create(name: 'user', email: 'user@email.com', password: '123456')
+  end
+  scenario 'with valid email and password' do
+    sign_up_with 'user@email.com', '123456'
+    expect(page).to have_content('Sign out')
+  end
+end
+
+def sign_up_with(email, password)
+  visit new_user_session_path
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  click_button 'Log in'
+end
+
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,13 +5,17 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'capybara/rspec'
+
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
